1af9b9867efd41e1fc0a8e88e61fd4ea274bcca3
Gubshig <ytjk060704@gmail.com>
2021-05-09 17:15:00 +0900

    20210509

--- citAndroidProject/app/src/main/java/com/example/test/MainPage/StudentMainActivity.java
+++ citAndroidProject/app/src/main/java/com/example/test/MainPage/StudentMainActivity.java
@@ -3,14 +3,21 @@ package com.example.test.MainPage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.test.R;
 
 public class StudentMainActivity extends AppCompatActivity {
 
+    private Button cNfcGetOnButton;
+    private Button cNfcGetOffButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main);
+
+        cNfcGetOffButton = findViewById(R.id.nfc_get_on);
+        cNfcGetOffButton = findViewById(R.id.nfc_get_off);
     }
 }
